d71ca14f842622751c277391d5a8680e90753f6f
St1ckySmash <99824309+St1ckySmash@users.noreply.github.com>
2024-02-01 21:02:27 +0400

    fix - player falling

--- Assets/_Source/Movement/PlayerMover.cs
+++ Assets/_Source/Movement/PlayerMover.cs
@@ -5,41 +5,38 @@ public class PlayerMover : MonoBehaviour
 	[SerializeField] private Rigidbody2D rb;
 	[SerializeField] private float movementSpeed = 0.6f;
 	[SerializeField] private float runningSpeedCoefficent = 1.5f;
-	private Vector2 movementVector;
+	private Vector2 _movementVector;
 
-	private void GetMovementVector()
-	{
-		movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-	}
-
-	public void Move()
-	{
-		GetMovementVector();
-		if (MathF.Abs(movementVector.x) > 0)
-		{
-			if (Input.GetKey(KeyCode.LeftShift))
-			{
-				rb.velocity = movementVector * movementSpeed * runningSpeedCoefficent;
-
-			}
-			else
-			{
-				rb.velocity = movementVector * movementSpeed;
-			}
-
-		}
-		else
-		{
-			rb.velocity = Vector2.zero;
-		}
-	}
-	// void Start()
-	// {
-	// rb = GetComponent<Rigidbody2D>();
-	// }
 	void Update()
 	{
 		Move();
 	}
 
+	private void GetMovementVector()
+	{
+		_movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+	}
+
+	public void Move()
+	{
+		GetMovementVector();
+		if (MathF.Abs(_movementVector.x) > 0)
+		{
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+				rb.velocity = new Vector2(_movementVector.x * movementSpeed * runningSpeedCoefficent, rb.velocity.y);
+
+			}
+			else
+			{
+				rb.velocity = new Vector2(_movementVector.x * movementSpeed, rb.velocity.y);
+
+			}
+
+		}
+		else
+		{
+			rb.velocity = new Vector2(0, rb.velocity.y);
+		}
+	}
 }
