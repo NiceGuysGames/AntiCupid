2116f04d4b75c41582eac55fe4ddf51cef93379e
Alexandr Gyach <as.gyach@gmail.com>
2024-02-01 15:05:13 +0300

    feat - added canvas and dialogue window

---
+++ Assets/_Source/DialogueSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
