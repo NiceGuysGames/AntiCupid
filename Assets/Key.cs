03e283377578b060605aa0e1cf1adf23ed99892c
maxim <Biarslanov.Maksim@gmail.com>
2024-02-01 07:26:54 +0300

    feat- added key-door script

--- Assets/Key.cs
+++ Assets/Key.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Key : MonoBehaviour
 {
     public GameObject door;
-
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        if (col.CompareTag("Player"))
         {
-            Debug.Log("picked");
-
+            Debug.Log("Key earned");
+            door.GetComponent<Collider2D>().enabled = false;
             this.gameObject.SetActive(false);
         }
     }
