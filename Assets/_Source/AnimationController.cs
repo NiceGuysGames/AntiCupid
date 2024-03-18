c9c5ec2d08f2e10fc4ff755e95586464f2621aa6
Alexandr Gyach <as.gyach@gmail.com>
2024-03-18 19:57:08 +0300

    fix - naming

--- Assets/_Source/AnimationController.cs
+++ Assets/_Source/AnimationController.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public static class AnimationController
 {
-    public static string _currentAnimation { get; private set; }
+    public static string CurrentAnimation { get; private set; }
     
     public static void ChangeAnimation(string animation, Animator animator)
     {
-        if (_currentAnimation == animation)
+        if (CurrentAnimation == animation)
         {
             return;
         }
 		
         animator.Play(animation);
-        _currentAnimation = animation;
+        CurrentAnimation = animation;
     }
 }
